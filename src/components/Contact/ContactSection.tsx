5f337b43812bdcd3d2cc43b50eda23a1a0c503aa
tisegenene <tisegenene@gmail.com>
2025-06-04 23:09:11 +0300

    make the contact section more compact by adjusting the spacing and layout.

--- src/components/Contact/ContactSection.tsx
+++ src/components/Contact/ContactSection.tsx
@@ -5,27 +5,27 @@ const ContactSection = () => {
   return (
     <section
       id="contact"
-      className="bg-secondary my-8 grid grid-cols-1 gap-16 rounded-4xl p-8 md:my-16 md:grid-cols-2 md:gap-8 lg:gap-12">
-      <div className="flex flex-col justify-between gap-8">
+      className="bg-secondary my-4 grid grid-cols-1 gap-8 rounded-2xl p-6 md:my-12 md:grid-cols-2 md:gap-6">
+      <div className="flex flex-col justify-between gap-4">
         <div>
-          <h3 className="text-neutral text-3xl font-bold">Let's Talk</h3>
-          <h4 className="text-accent text-2xl font-bold md:text-3xl">We'd love to help</h4>
-          <p className="text-neutral mt-8">
+          <h3 className="text-neutral text-2xl font-bold">Let's Talk</h3>
+          <h4 className="text-accent text-xl font-bold md:text-2xl">I'd love to help</h4>
+          <p className="text-neutral mt-4">
             Crafting innovative solutions to solve real-world problems
           </p>
         </div>
 
-        <div className="space-y-2">
-          <p className="text-neutral text-lg font-bold">Contact Information</p>
+        <div className="space-y-1">
+          <p className="text-neutral text-base font-bold">Contact Information</p>
           <a
-            href="mailto:johndoe@gmail.com"
+            href="mailto:tisegenene@gmail.com"
             className="text-neutral hover:text-accent flex items-center gap-1 font-light transition-colors duration-300">
-            <MsgIcon /> johndoe@gmail.com
+            <MsgIcon /> tisegenene@gmail.com
           </a>
           <a
-            href="tel:+92 3123456789"
+            href="tel:+251932508910"
             className="text-neutral hover:text-accent flex items-center gap-1 font-light transition-colors duration-300">
-            <PhoneIcon /> +92 3123456789
+            <PhoneIcon /> +251932508910
           </a>
         </div>
       </div>
